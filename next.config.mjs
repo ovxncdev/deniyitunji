65a50880e7073b7fdb70a6bd05d913332d52b979
ovxncdev <adeniyireal1icloud.com>
2026-03-31 07:58:22 -0700

    fix next.config conflicts

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-<<<<<<< HEAD
   async rewrites() {
     return [
       { source: '/proxysocket',         destination: '/proxysocket.html' },
@@ -15,8 +14,6 @@ const nextConfig = {
       { source: '/proxysocket/redeem',  destination: '/proxysocket/redeem/index.html' },
     ]
   },
-=======
->>>>>>> 29eb44fba430ada276e121745d5b45d467e175e5
 }
 
 export default nextConfig
