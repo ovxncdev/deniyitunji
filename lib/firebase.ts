a622466c1c7a4edaffcc60c91fffe1062a7ca1f5
ovxncdev <adeniyireal1icloud.com>
2026-03-31 14:20:01 -0700

    fix firebase key parsing

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,11 +2,15 @@ import { initializeApp, getApps, cert } from 'firebase-admin/app'
 import { getFirestore } from 'firebase-admin/firestore'
 
 if (!getApps().length) {
+  const privateKey = process.env.FIREBASE_PRIVATE_KEY
+    ?.replace(/\\n/g, '\n')
+    ?.replace(/^"|"$/g, '') // remove surrounding quotes if any
+
   initializeApp({
     credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      privateKey,
     }),
   })
 }
