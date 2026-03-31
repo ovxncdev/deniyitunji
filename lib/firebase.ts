0deb50cc5d4d1ffa723ed395f248b2d5771fb1b7
ovxncdev <adeniyireal1icloud.com>
2026-03-31 15:12:01 -0700

    debug firebase key

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,9 +2,15 @@ import { initializeApp, getApps, cert } from 'firebase-admin/app'
 import { getFirestore } from 'firebase-admin/firestore'
 
 if (!getApps().length) {
-  const privateKey = process.env.FIREBASE_PRIVATE_KEY
-    ?.replace(/\\n/g, '\n')
-    ?.replace(/^"|"$/g, '') // remove surrounding quotes if any
+  const rawKey = process.env.FIREBASE_PRIVATE_KEY || ''
+  const privateKey = rawKey
+    .replace(/\\n/g, '\n')
+    .replace(/^"|"$/g, '')
+
+  console.log('Firebase key starts:', privateKey.substring(0, 50))
+  console.log('Firebase key ends:', privateKey.substring(privateKey.length - 30))
+  console.log('Firebase project:', process.env.FIREBASE_PROJECT_ID)
+  console.log('Firebase email:', process.env.FIREBASE_CLIENT_EMAIL)
 
   initializeApp({
     credential: cert({
