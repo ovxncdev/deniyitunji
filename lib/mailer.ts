43def19c7fdb79d98f6ca3166d69691ea1569bdd
ovxncdev <adeniyireal1icloud.com>
2026-04-03 04:35:06 -0700

    strip spaces from gmail app password

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_APP_PASSWORD,
+    pass: (process.env.GMAIL_APP_PASSWORD || '').replace(/\s/g, ''),
   },
 })
 
